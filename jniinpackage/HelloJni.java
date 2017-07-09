0c6d5272b16eab3cb7dade05559f6bc68e3b02d5
Fang-Lin He <fanglin.he.ms@gmail.com>
2017-07-09 20:48:11 +0800

    implement java code

---
+++ jniinpackage/HelloJni.java
@@ -0,0 +1,16 @@
+package jniinpackage;
+
+public class HelloJni
+{
+  static
+  {
+    System.loadLibrary("hello");
+  }
+
+  private native void sayHello();
+
+  public static void main(String[] args)
+  {
+    new HelloJni().sayHello();
+  }
+}
